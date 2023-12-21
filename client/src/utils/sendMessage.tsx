9f1c7c54c9608045297b70283a2793a44122c158
Ankit Choudhary <sign.ankit14@gmail.com>
2023-12-21 19:17:33 +0530

    :sparkles: Add send message functionality

---
+++ client/src/utils/sendMessage.tsx
@@ -0,0 +1,18 @@
+import getPushUser from "./getPushUser";
+
+export default async function sendMessage(
+	bobAccount: string | null,
+	message: string
+) {
+	//bobAccount can be wallet address or group chat id
+	try {
+		const user: any = await getPushUser();
+		const aliceMessagesBob = await user.chat.send(bobAccount, {
+			type: "Text",
+			content: message,
+		});
+		console.log(aliceMessagesBob);
+	} catch (e) {
+		console.log(e);
+	}
+}
