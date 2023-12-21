7b4b422e6c2022496602d339dbe1a8b274defda1
Ankit Choudhary <sign.ankit14@gmail.com>
2023-12-22 00:41:42 +0530

    :sparkles: Add storing cashback functionality in smart contract

--- client/contracts/ModePay.sol
+++ client/contracts/ModePay.sol
@@ -6,15 +6,23 @@ contract Register {
 }
 
 contract ModePay {
-
     address public owner;
     mapping(uint256 => Split[]) public splits;
+    mapping(address => uint256) public cashback;
 
-    event SplitCreated(uint256 groupId, uint256 splitId, uint256 amount, address[] participants);
+    event SplitCreated(
+        uint256 groupId,
+        uint256 splitId,
+        uint256 amount,
+        address[] participants
+    );
     event PaymentMade(uint256 groupId, uint256 splitId, address participant);
-    event FundsTransferred(address indexed sender, address indexed recipient, uint256 amount);
+    event FundsTransferred(
+        address indexed sender,
+        address indexed recipient,
+        uint256 amount
+    );
     event RewardTransferred(address indexed sender, uint256 amount);
-
 
     struct Split {
         uint256 splitId;
@@ -31,7 +39,7 @@ contract ModePay {
     // constructor() {
     //     Register sfsContract = Register(0xBBd707815a7F7eb6897C7686274AFabd7B579Ff6);
     //     owner = msg.sender;
-    //     sfsContract.register(owner); 
+    //     sfsContract.register(owner);
     // }
 
     modifier splitExists(uint256 groupId, uint256 splitId) {
@@ -39,38 +47,57 @@ contract ModePay {
         _;
     }
 
-    modifier notPaid(uint256 groupId, uint256 splitId, address participant) {
-        require(!splits[groupId][splitId].hasPaid[participant], "Participant has already paid");
+    modifier notPaid(
+        uint256 groupId,
+        uint256 splitId,
+        address participant
+    ) {
+        require(
+            !splits[groupId][splitId].hasPaid[participant],
+            "Participant has already paid"
+        );
         _;
     }
 
-    function transferToAddress(address payable recipient, uint256 amount) public payable {
-        require(msg.value == amount, "Amount sent must match the specified amount");
+    function transferToAddress(
+        address payable recipient,
+        uint256 amount
+    ) public payable {
+        require(
+            msg.value == amount,
+            "Amount sent must match the specified amount"
+        );
         recipient.transfer(amount);
         emit FundsTransferred(msg.sender, recipient, amount);
         uint256 reward = getRandomNumber();
         address payable sender = payable(msg.sender);
-        if(address(this).balance>reward)
-            sender.transfer(reward);
-        else
-            reward = 0;
-        emit RewardTransferred(msg.sender, reward);     
+        if (address(this).balance > reward) sender.transfer(reward);
+        else reward = 0;
+        cashback[msg.sender] = cashback[msg.sender] + reward;
+        emit RewardTransferred(msg.sender, reward);
     }
 
-    receive() external payable {
-    }
+    receive() external payable {}
 
     function getContractBalance() external view returns (uint256) {
         return address(this).balance;
     }
 
     function getRandomNumber() internal view returns (uint256) {
-        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.timestamp,block.difficulty, msg.sender)));
+        uint256 randomNumber = uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.difficulty, msg.sender)
+            )
+        );
         return randomNumber % 65595826461684; // Add 1 to the upper bound to include it in the range
     }
 
-
-    function createSplit(uint256 groupId, uint256 amount, string memory reason ,address[] calldata participants) external {
+    function createSplit(
+        uint256 groupId,
+        uint256 amount,
+        string memory reason,
+        address[] calldata participants
+    ) external {
         require(amount > 0, "Amount must be greater than 0");
         require(participants.length > 0, "Participants list must not be empty");
 
@@ -82,25 +109,43 @@ contract ModePay {
         newSplit.amount = amount;
         newSplit.reason = reason;
         newSplit.owner = msg.sender;
-        newSplit.perShare = amount / (participants.length+1);
-        newSplit.amountLeft = amount - amount / (participants.length+1);
+        newSplit.perShare = amount / (participants.length + 1);
+        newSplit.amountLeft = amount - amount / (participants.length + 1);
         newSplit.participants = participants;
 
         emit SplitCreated(groupId, splitId, amount, participants);
     }
 
-    function makePayment(uint256 groupId, uint256 splitId) external splitExists(groupId, splitId) notPaid(groupId, splitId, msg.sender) payable {
-        require(msg.value == splits[groupId][splitId].perShare, "Incorrect payment amount");
+    function makePayment(
+        uint256 groupId,
+        uint256 splitId
+    )
+        external
+        payable
+        splitExists(groupId, splitId)
+        notPaid(groupId, splitId, msg.sender)
+    {
+        require(
+            msg.value == splits[groupId][splitId].perShare,
+            "Incorrect payment amount"
+        );
 
-        require(splits[groupId][splitId].hasPaid[msg.sender] == false,"Already paid");
+        require(
+            splits[groupId][splitId].hasPaid[msg.sender] == false,
+            "Already paid"
+        );
 
         transferToAddress(payable(splits[groupId][splitId].owner), msg.value);
 
-        splits[groupId][splitId].amountLeft = splits[groupId][splitId].amountLeft - msg.value;
+        splits[groupId][splitId].amountLeft =
+            splits[groupId][splitId].amountLeft -
+            msg.value;
 
         splits[groupId][splitId].hasPaid[msg.sender] = true;
 
-        splits[groupId][splitId].splitClose = true;
+        if (splits[groupId][splitId].amountLeft == 0) {
+            splits[groupId][splitId].splitClose = true;
+        }
 
         emit PaymentMade(groupId, splitId, msg.sender);
     }
@@ -109,38 +154,64 @@ contract ModePay {
         return splits[groupId].length;
     }
 
-    function getParticipants(uint256 groupId, uint256 splitId) external view splitExists(groupId, splitId) returns (address[] memory) {
+    function getParticipants(
+        uint256 groupId,
+        uint256 splitId
+    ) external view splitExists(groupId, splitId) returns (address[] memory) {
         return splits[groupId][splitId].participants;
     }
 
-    function getAmount(uint256 groupId, uint256 splitId) external view splitExists(groupId, splitId) returns (uint256) {
+    function getAmount(
+        uint256 groupId,
+        uint256 splitId
+    ) external view splitExists(groupId, splitId) returns (uint256) {
         return splits[groupId][splitId].amount;
     }
 
-    function getAmountLeft(uint256 groupId, uint256 splitId) external view splitExists(groupId, splitId) returns (uint256) {
+    function getAmountLeft(
+        uint256 groupId,
+        uint256 splitId
+    ) external view splitExists(groupId, splitId) returns (uint256) {
         return splits[groupId][splitId].amountLeft;
     }
 
-
-    function getPerShare(uint256 groupId, uint256 splitId) external view splitExists(groupId, splitId) returns (uint256) {
+    function getPerShare(
+        uint256 groupId,
+        uint256 splitId
+    ) external view splitExists(groupId, splitId) returns (uint256) {
         return splits[groupId][splitId].perShare;
     }
 
-
-    function getReason(uint256 groupId, uint256 splitId) external view splitExists(groupId, splitId) returns (string memory) {
+    function getReason(
+        uint256 groupId,
+        uint256 splitId
+    ) external view splitExists(groupId, splitId) returns (string memory) {
         return splits[groupId][splitId].reason;
     }
 
-    function hasPaid(uint256 groupId, uint256 splitId, address participant) external view splitExists(groupId, splitId) returns (bool) {
+    function hasPaid(
+        uint256 groupId,
+        uint256 splitId,
+        address participant
+    ) external view splitExists(groupId, splitId) returns (bool) {
         return splits[groupId][splitId].hasPaid[participant];
     }
 
-    function getSplitStatus(uint256 groupId, uint256 splitId) external view splitExists(groupId, splitId) returns (bool) {
+    function getSplitStatus(
+        uint256 groupId,
+        uint256 splitId
+    ) external view splitExists(groupId, splitId) returns (bool) {
         return splits[groupId][splitId].splitClose;
     }
 
-    function getSplitOwner(uint256 groupId, uint256 splitId) external view splitExists(groupId, splitId) returns (address) {
+    function getSplitOwner(
+        uint256 groupId,
+        uint256 splitId
+    ) external view splitExists(groupId, splitId) returns (address) {
         return splits[groupId][splitId].owner;
     }
 
+    function getCashBack() external view returns (uint256) {
+        return cashback[msg.sender];
+    }
 }
