5ce38c2fa6fa8adb01d5384710688ee07bced137
Karan <karanpargal007@gmail.com>
2023-12-23 00:24:09 +0530

    Push gd

--- client/src/components/AddGroupModal.tsx
+++ client/src/components/AddGroupModal.tsx
@@ -102,14 +102,24 @@ export default function AddGroupModal({
 
   const [showlist, setShowlist] = useState(false);
   const [groupName, setGroupName] = useState("");
+  const [description, setDescription] = useState("");
 
   function handleGroupNameChange(event: any) {
     // console.log(event.target.value);
     setGroupName(event.target.value);
   }
 
-  function handleCreateGroup(addressesArray: string[], groupName: string) {
-    createGroup(addressesArray, groupName);
+  function handleDescriptionChange(event: any) {
+    // console.log(event.target.value);
+    setDescription(event.target.value);
+  }
+
+  function handleCreateGroup(
+    addressesArray: string[],
+    groupName: string,
+    description: string
+  ) {
+    createGroup(addressesArray, groupName, description);
     toggleRefreshGroup();
     onClose();
     toast("Group Created");
@@ -146,7 +156,7 @@ export default function AddGroupModal({
     e.preventDefault();
     if (groupName.length > 0 && selectedFriends.length > 0) {
       console.log(selectedFriends, groupName);
-      handleCreateGroup(selectedFriends, groupName);
+      handleCreateGroup(selectedFriends, groupName, description);
     } else {
       alert(
         "Please enter the group name and make sure that you have atleast one member selected"
@@ -227,6 +237,17 @@ export default function AddGroupModal({
                   />
                   <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                     Group Name
+                  </label>
+                </div>
+                <div className="relative h-11 w-full min-w-[200px]">
+                  <input
+                    className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                    placeholder=" "
+                    onChange={handleDescriptionChange}
+                    value={groupName}
+                  />
+                  <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    Group Description
                   </label>
                 </div>
               </div>
--- client/src/utils/createGroup.tsx
+++ client/src/utils/createGroup.tsx
@@ -1,22 +1,23 @@
 import getPushUser from "./getPushUser";
 
 export default async function createGroup(
-	walletAddresses: string[] | null,
-	groupName: string
+  walletAddresses: string[] | null,
+  groupName: string,
+  description: string
 ) {
-	try {
-		console.log(walletAddresses);
-		const user: any = await getPushUser();
-		console.log(user);
-		const newGroup = await user.chat.group.create(groupName, {
-			description: "Group Description",
-			image: "groupImage",
-			members: walletAddresses,
-			admins: [],
-			private: false,
-		});
-		console.log(newGroup);
-	} catch (e) {
-		console.log(e);
-	}
+  try {
+    console.log(walletAddresses);
+    const user: any = await getPushUser();
+    console.log(user);
+    const newGroup = await user.chat.group.create(groupName, {
+      description: description,
+      image: "groupImage",
+      members: walletAddresses,
+      admins: [],
+      private: false,
+    });
+    console.log(newGroup);
+  } catch (e) {
+    console.log(e);
+  }
 }
